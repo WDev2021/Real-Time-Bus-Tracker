c3b16598a107a434f21bfc5b658d91a9309e7751
Jaime <77018423+WDev2021@users.noreply.github.com>
2021-05-15 16:34:19 -0400

    Add files via upload

--- mapanimation.js
+++ mapanimation.js
@@ -2,16 +2,67 @@ var map;
 var markers = [];
 
 // load map
-function init(){
-	var myOptions = {
-		zoom      : 14,
-		center    : { lat:42.353350,lng:-71.091525},
-		mapTypeId : google.maps.MapTypeId.ROADMAP
-	};
-	var element = document.getElementById('map');
-  	map = new google.maps.Map(element, myOptions);
-  	addMarkers();
-}
+mapboxgl.accessToken = 'pk.eyJ1Ijoid2ViZGV2MjAyMSIsImEiOiJja29od2N0aDkwcWNlMndvYTVveXU2M2xzIn0.xQfYLQKMYZUPHrT6DLL-3A';
+var map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/mapbox/streets-v11',
+	center: [-71.094915, 42.360175],
+	zoom: 12.7
+	
+});
+
+map.on('load', function () {
+	map.addSource('route', {
+		'type': 'geojson',
+		'data': {
+			'type': 'Feature',
+			'properties': {},
+			'geometry': {
+				'type': 'LineString',
+				'coordinates': [
+				[-71.083892, 42.329903],
+				[-71.082641, 42.330967],
+				[-71.081259, 42.332322],
+				[-71.079590, 42.332025],
+				[-71.076352, 42.331676],
+				[-71.073566, 42.333841],
+				[-71.074944, 42.334976],
+				[-71.076949, 42.336618],
+				[-71.080341, 42.339470],
+				[-71.083089, 42.341566],
+				[-71.084232, 42.342467],
+				[-71.087082, 42.345529], 
+				[-71.093729, 42.359244],
+				[-71.094915, 42.360175],
+				[-71.0958, 42.360698],
+				[-71.099558, 42.362953],
+				[-71.103476, 42.365248],
+				[-71.106067, 42.366806],
+				[-71.108717, 42.368355],
+				[-71.110799, 42.369192],
+				[-71.113095, 42.370218],
+				[-71.115476, 42.372085],
+				[-71.117585, 42.373016],
+				[-71.11744079074705, 42.373143738772235],
+				]
+			}
+		}
+	});
+
+	map.addLayer({
+		'id': 'route',
+		'type': 'line',
+		'source': 'route',
+		'layout': {
+			'line-join': 'round',
+			'line-cap': 'round'
+		},
+		'paint': {
+			'line-color': '#888',
+			'line-width': 8
+		}
+	});
+});
 
 // Add bus markers to map
 async function addMarkers(){
@@ -44,7 +95,7 @@ async function getBusLocations(){
 
 function addMarker(bus){
 	var icon = getIcon(bus);
-	var marker = new google.maps.Marker({
+	var marker = new mapboxgl.Marker({
 	    position: {
 	    	lat: bus.attributes.latitude, 
 	    	lng: bus.attributes.longitude
@@ -83,4 +134,4 @@ function getMarker(id){
 	return marker;
 }
 
-window.onload = init;
+addMarkers();
